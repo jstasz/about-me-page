f815d3de834c8c5a72810287e550b46d77905392
Justyna <jszajuk@gmail.com>
2022-07-26 12:04:10 +0200

    skills icons - better solution for opacity animation

--- src/js/script.js
+++ src/js/script.js
@@ -5,19 +5,14 @@ const skillsIcons = document.querySelector(".about-me__skills-icons");
 // SKILLS ANIMATION
 
 const handleHover = function (e) {
-	if (
-		e.target.classList.contains("fa-brands") ||
-		e.target.classList.contains("icon-name")
-	) {
-		const touchedIcon = e.target.closest(".skills-icon");
-		const siblings = touchedIcon
-			.closest(".about-me__skills-icons")
-			.querySelectorAll(".skills-icon");
+	const touchedIcon = e.target.closest(".skills-icon");
+	const siblings = touchedIcon
+		.closest(".about-me__skills-icons")
+		.querySelectorAll(".skills-icon");
 
-		siblings.forEach((icon) => {
-			if (icon !== touchedIcon) icon.style.opacity = this;
-		});
-	}
+	siblings.forEach((icon) => {
+		if (icon !== touchedIcon) icon.style.opacity = this;
+	});
 };
 
 skillsIcons.addEventListener("mouseover", handleHover.bind(0.3));
