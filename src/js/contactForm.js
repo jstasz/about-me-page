68272bd1cd98d5cb97f2b8f11afdbbff9c0fe4b3
justyna b <103330689+jstasz@users.noreply.github.com>
2023-09-13 12:34:15 +0200

    Update contactForm.js

--- src/js/contactForm.js
+++ src/js/contactForm.js
@@ -1,13 +1,13 @@
 const contactForm = document.getElementById("contactForm");
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCnG6166uck1qU-qbDxEfH7vzP_kVDib8E",
-	authDomain: "just-bro-contact.firebaseapp.com",
-	projectId: "just-bro-contact",
-	storageBucket: "just-bro-contact.appspot.com",
-	messagingSenderId: "104152681993",
-	appId: "1:104152681993:web:f79495f1c504c58655f2d8"
-  };
+  apiKey: "AIzaSyBmceSdg54pFrLJVzhWFEi-w1hKQmHgS5k",
+  authDomain: "contact-messages-72b87.firebaseapp.com",
+  projectId: "contact-messages-72b87",
+  storageBucket: "contact-messages-72b87.appspot.com",
+  messagingSenderId: "5609323308",
+  appId: "1:5609323308:web:ed2c610824295844070a89"
+};
 
 const getInputValue = function (id) {
 	return document.getElementById(id).value;
@@ -23,7 +23,7 @@ const resetContactForm = function () {
 };
 
 async function sendContactMessage(name, email, message) {
-	await fetch(`https://just-bro-contact-default-rtdb.firebaseio.com/messagess.json`, {
+	await fetch(`https://contact-messages-72b87-default-rtdb.firebaseio.com/messagess.json`, {
 	  method: 'POST',
 	  body: JSON.stringify({name: name, email: email, message: message}),
 	  headers: {
